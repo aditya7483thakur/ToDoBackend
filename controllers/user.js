5c97708d215874afbe41eac1b0d1fc89f077cd87
Aditya <adityakumarji74@gmail.com>
2023-12-16 12:55:46 +0530

    cookie commit

--- controllers/user.js
+++ controllers/user.js
@@ -54,5 +54,7 @@ export const logout = (req, res) => {
   res.clearCookie("token").json({
     success: true,
     message: "Logged out successfully",
+    sameSite: "none",
+    secure: true,
   });
 };
